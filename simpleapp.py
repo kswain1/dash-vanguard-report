d916f54b87057b02e5c6361596deb091e8c6a3b0
kehlin swain <kehlinswain@kehlins-MacBook-Pro.local>
2018-07-31 08:52:22 -0500

    updated code depedencies

--- simpleapp.py
+++ simpleapp.py
@@ -1,7 +1,12 @@
+# coding: utf-8
 
 import dash
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
+import os
 
 app = dash.Dash(__name__)
 server = app.server
